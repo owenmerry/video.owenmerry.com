55ad7f5acfcbf709fefb651f51eb43348b7a6d76
Owen Merry <owenmerry@Owens-MacBook-Air.local>
2020-06-01 18:28:09 +0100

    added video multi

---
+++ app.js
@@ -0,0 +1,127 @@
+// reads in our .env file and makes those values available as environment variables
+require('dotenv').config();
+ 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+var fs = require('fs');
+
+ 
+// create an instance of an express app
+const app = express();
+const server = require('https').createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}, app);
+const io = require('socket.io').listen(server);
+ 
+
+
+
+
+//socket io calls
+
+//variables
+const players = {};
+const conversation = {};
+const users = {
+  online: []
+}
+ 
+
+io.on('connection', function (socket) {
+  console.log('a user connected: ', socket.id);
+  // send the user id
+  socket.emit('connected', socket.id);
+
+  //setup init conversation   
+  conversation[socket.id] = {
+      users: {},
+  };
+
+  // update all other players of the new player
+  //socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  users.online.push(socket.id);
+  io.emit('allUsers', users);
+  io.emit('startUser', users);
+  socket.broadcast.emit('newUser', socket.id);
+ 
+  // when a player disconnects, remove them from our user object
+  socket.on('disconnect', function () {
+    const index = users.online.indexOf(socket.id);
+    if (index > -1) {
+      users.online.splice(index, 1);
+    }
+    io.emit('allUsers', users);
+    socket.broadcast.emit('removeUser', socket.id);
+  });
+
+
+  //video sockets
+
+  //offer
+  socket.on('sendOffer', function (offerData) {
+    console.log('send offer');
+    conversation[socket.id].users[offerData.to] = {
+      to: offerData.to,
+      from: offerData.from,
+      offer: offerData.offer,
+    };
+    // emit to user for answer
+    io.to(offerData.to).emit('reciveOffer', conversation[socket.id].users[offerData.to]);
+  });
+
+  //answer
+  socket.on('sendAnswer', function (answerData) {
+    console.log('send answer');
+      conversation[answerData.to].users[socket.id].answer = answerData.answer;
+
+      // emit to user for answer
+      io.to(answerData.to).emit('reciveAnswer', conversation[answerData.to].users[socket.id]);
+  });
+
+});
+
+
+
+
+
+
+
+ 
+// update express settings
+app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(cookieParser());
+ 
+// require passport auth
+require('./auth/auth');
+
+ 
+app.get('/game.html', function (req, res) {
+  res.sendFile(__dirname + '/public/game.html');
+});
+ 
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+ 
+// catch all other routes
+app.use((req, res, next) => {
+  res.status(404).json({ message: '404 - Not Found' });
+});
+ 
+// handle errors
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(err.status || 500).json({ error: err.message });
+});
+ 
+server.listen(process.env.PORT || 8443, () => {
+  console.log(`Server started on port ${process.env.PORT || 8443}`);
+});
---
+++ public/assets/js/video.js
@@ -0,0 +1,21 @@
+var video_out = document.getElementById("vid-box");
+
+function login(form) {
+	var phone = window.phone = PHONE({
+	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+	    publish_key   : 'pub-c-08e56e24-7ad4-4b09-bda7-0ca2b0c5227a',
+	    subscribe_key : 'sub-c-92c8e814-a1a6-11ea-8e71-f2b83ac9263d',
+	});	
+	phone.ready(function(){ form.username.style.background="#55ff5b"; });
+	phone.receive(function(session){
+	    session.connected(function(session) { video_out.appendChild(session.video); });
+	    session.ended(function(session) { video_out.innerHTML=''; });
+	});
+	return false; 	// So the form does not submit.
+}
+
+function makeCall(form){
+	if (!window.phone) alert("Login First!");
+	else phone.dial(form.number.value);
+	return false;
+}
